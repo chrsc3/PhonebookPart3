d8ba397e6fa3c668c4edcf57e229c2365ea7bda6
Christian Cabezas <christianc3c3@gmail.com>
2024-08-18 23:17:30 -0400

    Guardado En la Base De Datos implementado

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+require("dotenv").config();
+const Person = require("./models/person");
 const app = express();
+
 morgan.token("content", function (req, res) {
   return JSON.stringify(req.body);
 });
-app.use(express.json());
+
 app.use(cors());
 
 app.use(
@@ -15,89 +18,88 @@ app.use(
   )
 );
 app.use(express.static("dist"));
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-app.get("/", (request, response) => {
-  response.send("<h1>PhoneBoock</h1>");
+app.use(express.json());
+
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 app.get("/info", (request, response) => {
   let date = new Date();
-  response.send(
-    `<p>phoneboock has info for: ${persons.length} </p> <br/> <p> ${date} </p>`
-  );
+  let personsArray = [];
+  Person.find({}).then((person) => {
+    personsArray = Array.isArray(person) ? person : [person];
+    response.send(
+      `<p>phoneboock has info for: ${personsArray.length} </p> <br/> <p> ${date} </p>`
+    );
+  });
 });
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
 
-  response.status(204).end();
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
-
-const generateId = () => {
-  const radomId = Math.floor(Math.random() * 1000);
-  return radomId;
-};
-
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  if (!(body.name && body.number)) {
-    return response.status(400).json({
-      error: "No Name or No Number",
-    });
+  if (body === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  if (persons.some((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-const PORT = process.env.PORT || 3001;
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler);
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
